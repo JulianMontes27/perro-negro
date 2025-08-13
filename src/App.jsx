4f2b11551d222577ba9f8ddfee64426e3c23e6e6
Windows Julian Montes <julianmontesps4@gmail.com>
2025-08-13 14:36:36 -0500

    feat: added vercel analytics.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import { motion } from 'framer-motion'
-import { SpeedInsights } from '@vercel/speed-insights/react'
-import Header from './components/Header/Header'
-import Hero from './components/Hero/Hero'
-import EventInfo from './components/EventInfo/EventInfo'
-import WorldTour from './components/WorldTour/WorldTour'
-import Venue from './components/Venue/Venue'
-import Tickets from './components/Tickets/Tickets'
-import EventMap from './components/EventMap/EventMap'
-import Sponsors from './components/Sponsors/Sponsors'
-import OfficialSite from './components/OfficialSite/OfficialSite'
-import Footer from './components/Footer/Footer'
+import { motion } from "framer-motion";
+import { SpeedInsights } from "@vercel/speed-insights/react";
+import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
+import EventInfo from "./components/EventInfo/EventInfo";
+import WorldTour from "./components/WorldTour/WorldTour";
+import Venue from "./components/Venue/Venue";
+import Tickets from "./components/Tickets/Tickets";
+import EventMap from "./components/EventMap/EventMap";
+import Sponsors from "./components/Sponsors/Sponsors";
+import OfficialSite from "./components/OfficialSite/OfficialSite";
+import Footer from "./components/Footer/Footer";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
@@ -18,13 +19,13 @@ function App() {
       <div id="hero">
         <Hero />
       </div>
-      
+
       {/* Pattern Separator - Infinite Carousel */}
       <section className="relative w-full h-12 md:h-16 lg:h-20 overflow-hidden">
         <motion.div
           className="flex h-full"
           animate={{
-            x: [0, -50 + '%']
+            x: [0, -50 + "%"],
           }}
           transition={{
             x: {
@@ -40,11 +41,12 @@ function App() {
               key={index}
               className="flex-shrink-0 h-full"
               style={{
-                width: '150px',
-                backgroundImage: 'url(https://jtfcfsnksywotlbsddqb.supabase.co/storage/v1/object/public/perro-negro//pattern3.jpg)',
-                backgroundSize: '100% 100%',
-                backgroundPosition: 'center',
-                backgroundRepeat: 'no-repeat'
+                width: "150px",
+                backgroundImage:
+                  "url(https://jtfcfsnksywotlbsddqb.supabase.co/storage/v1/object/public/perro-negro//pattern3.jpg)",
+                backgroundSize: "100% 100%",
+                backgroundPosition: "center",
+                backgroundRepeat: "no-repeat",
               }}
             />
           ))}
@@ -53,17 +55,18 @@ function App() {
               key={`duplicate-${index}`}
               className="flex-shrink-0 h-full"
               style={{
-                width: '150px',
-                backgroundImage: 'url(https://jtfcfsnksywotlbsddqb.supabase.co/storage/v1/object/public/perro-negro//pattern3.jpg)',
-                backgroundSize: '100% 100%',
-                backgroundPosition: 'center',
-                backgroundRepeat: 'no-repeat'
+                width: "150px",
+                backgroundImage:
+                  "url(https://jtfcfsnksywotlbsddqb.supabase.co/storage/v1/object/public/perro-negro//pattern3.jpg)",
+                backgroundSize: "100% 100%",
+                backgroundPosition: "center",
+                backgroundRepeat: "no-repeat",
               }}
             />
           ))}
         </motion.div>
       </section>
-      
+
       <div id="event-info">
         <EventInfo />
       </div>
@@ -84,9 +87,9 @@ function App() {
       </div>
       <OfficialSite />
       <Footer />
-      <SpeedInsights />
+      <Analytics />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
